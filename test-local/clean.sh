e7b61e37d9c5c94bf56ded5b9dfb21ff9797a19f
Sebastian Kürten <sebastian@topobyte.de>
2023-09-11 12:21:53 +0200

    Add tests that work with locally published artifacts

---
+++ test-local/clean.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+pushd .
+cd test1
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean
+popd
+
+pushd .
+cd test2
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean
+popd
+
+pushd .
+cd test3
+pwd
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew clean
+popd
+
+pushd .
+cd test4
+pwd
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew clean
+popd
---
+++ test-local/test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+pushd .
+cd test1
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+popd
+
+pushd .
+cd test2
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+popd
+
+pushd .
+cd test3
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+popd
+
+pushd .
+cd test3
+pwd
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+popd
+
+pushd .
+cd test4
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+popd
+
+pushd .
+cd test4
+pwd
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+popd
