d7fe3af3cb64a9617ff41f2dcec961a39af4f3e6
Sebastian Kürten <sebastian@topobyte.de>
2023-09-11 12:05:07 +0200

    Add testing project with Gradle 7.6.1

---
+++ test/test3/core/src/main/java/com/example/Test.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.example.test.Version;
+
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(
+				"This application is using version " + Version.getVersion());
+	}
+
+}
